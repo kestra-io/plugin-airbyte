7d5399c7d0735cab64fcbe331e8a749e3b273b69
Ludovic DEHON <tchiot.ludo@gmail.com>
2023-02-27 16:23:56 +0100

    fix(tasks): configure properly http client for json deserialization

--- src/main/java/io/kestra/plugin/airbyte/AbstractAirbyteConnection.java
+++ src/main/java/io/kestra/plugin/airbyte/AbstractAirbyteConnection.java
@@ -12,7 +12,9 @@ import io.micronaut.http.MutableHttpRequest;
 import io.micronaut.http.client.DefaultHttpClientConfiguration;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.exceptions.HttpClientResponseException;
+import io.micronaut.http.client.netty.DefaultHttpClient;
 import io.micronaut.http.client.netty.NettyHttpClientFactory;
+import io.micronaut.http.codec.MediaTypeCodecRegistry;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -58,8 +60,13 @@ public abstract class AbstractAirbyteConnection extends Task implements DynamicT
 
     private static final NettyHttpClientFactory FACTORY = new NettyHttpClientFactory();
 
-    protected HttpClient client() throws IllegalVariableEvaluationException, MalformedURLException, URISyntaxException {
-        return FACTORY.createClient(URI.create(this.url).toURL(), new DefaultHttpClientConfiguration());
+    protected HttpClient client(RunContext runContext) throws IllegalVariableEvaluationException, MalformedURLException, URISyntaxException {
+        MediaTypeCodecRegistry mediaTypeCodecRegistry = runContext.getApplicationContext().getBean(MediaTypeCodecRegistry.class);
+
+        DefaultHttpClient client = (DefaultHttpClient) FACTORY.createClient(URI.create(this.url).toURL(), new DefaultHttpClientConfiguration());
+        client.setMediaTypeCodecRegistry(mediaTypeCodecRegistry);
+
+        return client;
     }
 
     protected <REQ, RES> HttpResponse<RES> request(RunContext runContext, MutableHttpRequest<REQ> request, Argument<RES> argument) throws HttpClientResponseException {
@@ -75,7 +82,7 @@ public abstract class AbstractAirbyteConnection extends Task implements DynamicT
                 request = request.basicAuth(runContext.render(this.username), runContext.render(this.password));
             }
 
-            try (HttpClient client = this.client()) {
+            try (HttpClient client = this.client(runContext)) {
                 return client.toBlocking().exchange(request, argument);
             }
         } catch (HttpClientResponseException e) {
