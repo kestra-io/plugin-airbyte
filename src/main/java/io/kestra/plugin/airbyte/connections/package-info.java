4ae7ba8b054eafea977d0f13c9f418349dab1fce
Loïc Mathieu <loikeseke@gmail.com>
2023-03-21 14:19:10 +0100

    feat: add plugin categories and icons

---
+++ src/main/java/io/kestra/plugin/airbyte/connections/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using Airbyte.\n" +
+    "Airbyte is an open-source data integration engine that helps you consolidate your data in your data warehouses, lakes and databases.",
+    categories = PluginSubGroup.PluginCategory.TOOL
+)
+package io.kestra.plugin.airbyte.connections;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
