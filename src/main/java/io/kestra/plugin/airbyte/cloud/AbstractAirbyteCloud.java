ec5414e7076a8cbceb3dd48749598428ec5b635c
Ludovic DEHON <tchiot.ludo@gmail.com>
2023-06-14 18:45:30 +0200

    feat(cloud): add a retry on some transitive error

--- src/main/java/io/kestra/plugin/airbyte/cloud/AbstractAirbyteCloud.java
+++ src/main/java/io/kestra/plugin/airbyte/cloud/AbstractAirbyteCloud.java
@@ -2,9 +2,13 @@ package io.kestra.plugin.airbyte.cloud;
 
 import com.airbyte.api.Airbyte;
 import com.airbyte.api.models.shared.Security;
+import com.airbyte.api.utils.HTTPClient;
+import com.airbyte.api.utils.HTTPRequest;
 import io.kestra.core.models.annotations.PluginProperty;
 import io.kestra.core.models.tasks.Task;
+import io.kestra.core.models.tasks.retrys.Exponential;
 import io.kestra.core.runners.RunContext;
+import io.kestra.core.utils.RetryUtils;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -12,8 +16,13 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 
 @SuperBuilder
 @ToString
@@ -28,7 +37,36 @@ public abstract class AbstractAirbyteCloud extends Task {
     String token;
 
     protected Airbyte client(RunContext runContext) throws Exception {
+        RetryUtils.Instance<HttpResponse<byte[]>, Exception> retry = runContext
+            .getApplicationContext()
+            .getBean(RetryUtils.class)
+            .of(
+                Exponential.builder()
+                    .delayFactor(2.0)
+                    .interval(Duration.ofSeconds(1))
+                    .maxInterval(Duration.ofSeconds(15))
+                    .maxAttempt(-1)
+                    .maxDuration(Duration.ofMinutes(5))
+                    .build(),
+                runContext.logger()
+            );
+
         return Airbyte.builder()
+            .setClient(request -> {
+                HttpClient client = HttpClient.newHttpClient();
+
+                HttpRequest req = request.build();
+
+                try {
+                    return retry
+                        .run(
+                            (httpResponse) -> httpResponse.statusCode() == 408,
+                            () -> client.send(req, HttpResponse.BodyHandlers.ofByteArray())
+                        );
+                } catch (Exception e) {
+                    throw new IOException(e);
+                }
+            })
             .setSecurity(new Security(runContext.render(this.token)))
             .build();
     }
