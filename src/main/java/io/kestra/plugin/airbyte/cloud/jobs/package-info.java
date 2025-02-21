7628e9b4bb0037e4d5b50404634598c4cbf5ceb2
brian.mulier <bmmulier@hotmail.fr>
2025-02-21 14:20:58 +0100

    fix: move package-info.java to the last package section for airbyte cloud jobs as inheritance is not a thing

--- src/main/java/io/kestra/plugin/airbyte/cloud/jobs/package-info.java
+++ src/main/java/io/kestra/plugin/airbyte/cloud/jobs/package-info.java
@@ -1,8 +1,8 @@
 @PluginSubGroup(
-    description = "This sub-group of plugins contains tasks for using Airbyte Cloud.\n" +
+    description = "This sub-group of plugins contains tasks for using Airbyte Cloud Jobs.\n" +
     "Airbyte is an open-source data integration engine that helps you consolidate your data in your data warehouses, data lakes and databases.",
     categories = PluginSubGroup.PluginCategory.TOOL
 )
-package io.kestra.plugin.airbyte.cloud;
+package io.kestra.plugin.airbyte.cloud.jobs;
 
 import io.kestra.core.models.annotations.PluginSubGroup;
