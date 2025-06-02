8f2e9542e9ef94a704992de386275282f99a4f58
Ludovic DEHON <tchiot.ludo@gmail.com>
2025-06-02 14:48:15 +0200

    chore(deps): bump com.airbyte api to 2.0.0 (#108)
    * feat: upgrade to latest sdk
    
    + add application auth
    
    * fix(airbyte): connectionId should not be null
    
    * chore(boyscout): use Property.ofValue
    
    * fix(airbyte): add a default access token URL
    
    ---------
    
    Co-authored-by: François Delbrayelle <fdelbrayelle@gmail.com>

--- src/main/java/io/kestra/plugin/airbyte/cloud/AbstractAirbyteCloud.java
+++ src/main/java/io/kestra/plugin/airbyte/cloud/AbstractAirbyteCloud.java
@@ -2,22 +2,22 @@ package io.kestra.plugin.airbyte.cloud;
 
 import com.airbyte.api.Airbyte;
 import com.airbyte.api.models.shared.SchemeBasicAuth;
+import com.airbyte.api.models.shared.SchemeClientCredentials;
 import com.airbyte.api.models.shared.Security;
-import io.kestra.core.models.annotations.PluginProperty;
+import com.airbyte.api.utils.SpeakeasyHTTPClient;
 import io.kestra.core.models.property.Property;
 import io.kestra.core.models.tasks.Task;
 import io.kestra.core.models.tasks.retrys.Exponential;
 import io.kestra.core.runners.RunContext;
 import io.kestra.core.utils.RetryUtils;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
-import java.net.http.HttpClient;
+import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
@@ -29,10 +29,28 @@ import java.time.Duration;
 @Getter
 @NoArgsConstructor
 public abstract class AbstractAirbyteCloud extends Task {
+    public static final String DEFAULT_TOKEN_URL = "https://api.airbyte.com/v1/applications/token";
     @Schema(
         title = "API key."
     )
-    Property<String> token;
+    private Property<String> token;
+
+    @Schema(
+        title = "Application Client ID."
+    )
+    private Property<String> clientId;
+
+    @Schema(
+        title = "Application Client Secret."
+    )
+    private Property<String> clientSecret;
+
+    @Schema(
+        title = "Token URL to get an access token from.",
+        description = "See: https://reference.airbyte.com/reference/createaccesstoken"
+    )
+    @Builder.Default
+    private Property<String> tokenURL = Property.ofValue(DEFAULT_TOKEN_URL);
 
     @Schema(
         title = "BasicAuth authentication username."
@@ -45,52 +63,64 @@ public abstract class AbstractAirbyteCloud extends Task {
     private Property<String> password;
 
     protected Airbyte client(RunContext runContext) throws Exception {
-        RetryUtils.Instance<HttpResponse<byte[]>, Exception> retry = new RetryUtils()
-            .of(
-                Exponential.builder()
-                    .delayFactor(2.0)
-                    .interval(Duration.ofSeconds(1))
-                    .maxInterval(Duration.ofSeconds(15))
-                    .maxAttempt(-1)
-                    .maxDuration(Duration.ofMinutes(5))
-                    .build(),
-                runContext.logger()
-            );
-
         Security security = new Security();
+
         if (this.token != null) {
             security.withBearerAuth(runContext.render(this.token).as(String.class).orElseThrow());
-        }
-
-        if (this.token != null) {
-            security.withBasicAuth(new SchemeBasicAuth(runContext.render(this.username).as(String.class).orElseThrow(),
-                runContext.render(this.password).as(String.class).orElseThrow()));
+        } else if (this.clientId != null && this.clientSecret != null) {
+            security.withClientCredentials(new SchemeClientCredentials(
+                runContext.render(this.clientId).as(String.class).orElseThrow(),
+                runContext.render(this.clientSecret).as(String.class).orElseThrow(),
+                runContext.render(this.tokenURL).as(String.class).orElse(DEFAULT_TOKEN_URL)
+            ));
+        } else {
+            security.withBasicAuth(new SchemeBasicAuth(
+                runContext.render(this.username).as(String.class).orElseThrow(),
+                runContext.render(this.password).as(String.class).orElseThrow()
+            ));
         }
 
         return Airbyte.builder()
-            .setClient(request -> {
-                HttpClient client = HttpClient.newHttpClient();
-
-                HttpRequest req = request.build();
-
-                try {
-                    return retry
-                        .run(
-                            (httpResponse) -> httpResponse.statusCode() == 408,
-                            () -> client.send(req, HttpResponse.BodyHandlers.ofByteArray())
-                        );
-                } catch (Exception e) {
-                    throw new IOException(e);
-                }
-            })
-            .setSecurity(security)
+            .client(new CustomHttpClient(runContext))
+            .security(security)
             .build();
     }
 
-    protected void validate(HttpResponse<byte[]> response) throws Exception {
+    public static class CustomHttpClient extends SpeakeasyHTTPClient {
+        private final RetryUtils.Instance<HttpResponse<InputStream>, Exception> retry;
+
+        public CustomHttpClient(RunContext runContext) {
+            retry = new RetryUtils()
+                .of(
+                    Exponential.builder()
+                        .delayFactor(2.0)
+                        .interval(Duration.ofSeconds(1))
+                        .maxInterval(Duration.ofSeconds(15))
+                        .maxAttempt(-1)
+                        .maxDuration(Duration.ofMinutes(5))
+                        .build(),
+                    runContext.logger()
+                );
+        }
+
+        @Override
+        public HttpResponse<InputStream> send(HttpRequest request) throws IOException, InterruptedException, URISyntaxException {
+            try {
+                return retry
+                    .run(
+                        (httpResponse) -> httpResponse.statusCode() == 408,
+                        () -> super.send(request)
+                    );
+            } catch (Exception e) {
+                throw new IOException(e);
+            }
+        }
+    }
+
+    protected void validate(HttpResponse<InputStream> response) throws Exception {
         if (response.statusCode() >= 400) {
             throw new Exception("Failed request with status " + response.statusCode() + " and body " +
-                new String(response.body(), StandardCharsets.UTF_8)
+                IOUtils.toString(response.body(), StandardCharsets.UTF_8)
             );
         }
     }
