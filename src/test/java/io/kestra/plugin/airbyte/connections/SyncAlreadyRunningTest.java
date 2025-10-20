62775a7f98a37548556b9b3ea7001b512283a56f
Malay Dewangan <malaydewangan310@gmail.com>
2025-10-20 10:55:14 +0530

    fix(tests): attempt to fix flaky test

--- src/test/java/io/kestra/plugin/airbyte/connections/SyncAlreadyRunningTest.java
+++ src/test/java/io/kestra/plugin/airbyte/connections/SyncAlreadyRunningTest.java
@@ -7,6 +7,8 @@ import io.kestra.core.junit.annotations.KestraTest;
 import io.kestra.plugin.airbyte.AbstractAirbyteConnectionTest;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
@@ -40,5 +42,19 @@ class SyncAlreadyRunningTest extends AbstractAirbyteConnectionTest {
 
         Sync.Output runOutput2 = second.run(runContext);
         assertThat(runOutput2.getAlreadyRunning(), is(true));
+
+        // we check status until the job is finished to make other tests start sync properly
+        if (firstOut.getJobId() != null) {
+            CheckStatus checkStatus = CheckStatus.builder()
+                .url(Property.ofValue(url))
+                .username(Property.ofValue(username))
+                .password(Property.ofValue(password))
+                .jobId(Property.ofValue(firstOut.getJobId().toString()))
+                .pollFrequency(Property.ofValue(Duration.ofSeconds(2)))
+                .maxDuration(Property.ofValue(Duration.ofMinutes(10)))
+                .build();
+
+            checkStatus.run(runContext);
+        }
     }
 }
